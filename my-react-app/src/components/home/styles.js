22f636adda2698cb30eb5492320fa5985f0300a4
EduardoHenriGC <eduardohenriqu3@hotmail.com>
2022-08-27 03:08:48 -0300

    primeiro push

--- my-react-app/src/components/home/styles.js
+++ my-react-app/src/components/home/styles.js
@@ -5,9 +5,9 @@ export const Heros = styled.div`
   .hero {
     background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)),
       url(${viper});
-    background-position: center;
+    background-position: top;
     background-size: cover;
-    height: 90vh;
+    height: 100vh;
     width: 100%;
   }
 `;
